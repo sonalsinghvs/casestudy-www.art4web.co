891d9cb0eacbd6b45f1cccf9b0cfd6a1f866ddd6
sonalsinghvs <49417594+sonalsinghvs@users.noreply.github.com>
2019-05-03 20:39:06 -0600

    quick push

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 var navButton = document.querySelector('.bar-4');
-navButton.addEventListener('click', function() {
-    console.log('clicky!!!');
-    document.querySelector('body')
-    .classList
-    .toggle('nav-open');
-});
+navButton.addEventListener('click', function() 
+{ console.log('clicky!!!');
+ document.querySelector('body')
+ .classList
+ .toggle('nav-open');
+});                       
+ 
+var closeButton = 
+    document.querySelector('.close-icon');
 
-var closeButton = document.querySelector('.close-icon');
-closeButton.addEventlistener('click', function() {
-    console.log('close it!');
-    document.querySelector('body')
-    .classList
-    .toggle('nav-open');
-});
+closeButton.addEventListener('click', function() {
+  console.log('close it!');
+  document.querySelector('body')
+ .classList
+ .toggle('nav-open');
+} );
+
